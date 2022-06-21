d35b8d72bb3c8dfde3db9e62aa2700279409b5a0
Anupreet Singh <72339688+realanupreet@users.noreply.github.com>
2022-06-21 20:48:33 +0530

    #Day20 of #100DaysOfCode

---
+++ postordertraversal.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+
+        # def getInOrderTraversal(root):
+        #     Write your code here.
+        if not root:
+            return []
+        else:
+            return self.postorderTraversal(root.left)+self.postorderTraversal(root.right)+[root.val]
+#     pass
