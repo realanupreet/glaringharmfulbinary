93199c32277ad670c86f10c0b840b969908f3864
Anupreet Singh <72339688+realanupreet@users.noreply.github.com>
2022-09-27 22:12:50 +0530

    #Day61 of #100DaysOfCode

---
+++ adddigits.py
@@ -0,0 +1,21 @@
+num = 0
+snum = str(num)
+lsum = []
+for n in snum:
+    lsum.append(int(n))
+print("hello")
+
+while len(lsum) > 1:
+    num = 0
+    for l in lsum:
+        num += l
+    snum = str(num)
+    lsum = []
+    for n in snum:
+        lsum.append(int(n))
+print(lsum[0])
+# break
+
+#     res = []
+#     for n in nn:
+#         res
