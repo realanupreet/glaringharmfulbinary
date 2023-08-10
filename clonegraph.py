837609c1cb5a15eecffe9abac8332e09d2e4cd71
Anupreet Singh <72339688+realanupreet@users.noreply.github.com>
2023-08-10 16:55:36 +0530

    add: clonegraph

---
+++ clonegraph.py
@@ -0,0 +1,31 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+
+class Solution(object):
+    def cloneGraph(self, node):
+        """
+        :type node: Node
+        :rtype: Node
+        """
+        if not node:
+            return node
+
+        q, clones = deque([node]), {node.val: Node(node.val, [])}
+        while q:
+            cur = q.popleft()
+            cur_clone = clones[cur.val]
+
+            for ngbr in cur.neighbors:
+                if ngbr.val not in clones:
+                    clones[ngbr.val] = Node(ngbr.val, [])
+                    q.append(ngbr)
+
+                cur_clone.neighbors.append(clones[ngbr.val])
+
+        return clones[node.val]
