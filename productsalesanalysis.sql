3309cee46e6a5c8fc1e96ad742de8c279f1c901e
Anupreet Singh <72339688+realanupreet@users.noreply.github.com>
2023-08-19 11:57:05 +0530

    add: productanalysis

---
+++ productsalesanalysis.sql
@@ -0,0 +1,12 @@
+SELECT
+    DISTINCT P.product_name,
+    S.year,
+    S.price
+FROM (
+        SELECT
+            DISTINCT product_id,
+            year,
+            price
+        FROM Sales
+    ) S
+    INNER JOIN Product AS P USING (product_id);
